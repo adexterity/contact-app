99cc1f4f4aff40baeceea9c228e1ad4472a9c046
suberu joseph <opensesame4me@gmail.com>
2023-03-21 10:07:49 +0100

    can add new contact and delete some contact too

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,21 +1,16 @@
 import React from "react";
 
-const ContactList = ({ contacts }) => {
-  const renderContactList = contacts.map((contact) => {
-    return (
-      <div>
-        <p>name: {contact.name}</p>
-        <p>email: {contact.email}</p>
-        <p>phone number: {contact.number}</p>
-      </div>
-    );
+import ContactCard from "./ContactCard";
+
+// this function displays the content of the state object
+const ContactList = (props) => {
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
+    return <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id} />;
   });
-  return (
-    <div className="ui celled list">
-      <h2>Contact List</h2>
-      {renderContactList}
-    </div>
-  );
+  return <div className="ui celled list">{renderContactList}</div>;
 };
 
 export default ContactList;
